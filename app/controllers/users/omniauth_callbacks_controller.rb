ece253cf4e505c636ed012cafd78300afacb28ce
James Zhan <zhiqiangzhan@gmail.com>
2012-12-18 11:27:11 +0800

    Add token_info to session.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,10 +1,9 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   
-  def auth
-    
+  def auth    
     @user = User.find_with_omniauth(request.env["omniauth.auth"], current_user)
-    fields = env["omniauth.auth"] || request.env['rack.auth']    
-    
+    fields = env["omniauth.auth"] || request.env['rack.auth']   
+    session['token_info'] = fields.extra.raw_info['token_info']    
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => fields["provider"].titleize) if is_navigational_format?
