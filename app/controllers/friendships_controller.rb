24e818b15ee166142d4fb77e862fc958d07bd13e
James Zhan <zhiqiangzhan@gmail.com>
2012-12-18 10:18:04 +0800

    Add friendships module.

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -12,31 +12,12 @@ class FriendshipsController < ApplicationController
     end
   end
 
-  # PUT /friendships/1
-  # PUT /friendships/1.json
-  def update
-    @friendship = Friendship.find(params[:id])
 
-    respond_to do |format|
-      if @friendship.update_attributes(params[:friendship])
-        format.html { redirect_to @friendship, notice: 'Friendship was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @friendship.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /friendships/1
-  # DELETE /friendships/1.json
   def destroy
-    @friendship = Friendship.find(params[:id])
+    @friendship = current_user.friendships.find(params[:id])
     @friendship.destroy
-
-    respond_to do |format|
-      format.html { redirect_to friendships_url }
-      format.json { head :no_content }
-    end
+    flash[:notice] = "Removed friendship."
+    redirect_to root_url
   end
+  
 end
